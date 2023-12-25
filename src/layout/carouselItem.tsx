dfd55c48db29dca93acb4aa1427a790e7d5acfb3
Alekanteri <sandor.basc@gmail.com>
2023-12-25 17:41:26 +0300

    sponsors block

--- src/layout/carouselItem.tsx
+++ src/layout/carouselItem.tsx
@@ -1,12 +1,32 @@
 import React from "react";
 import "../sass/layout/carousel.scss";
 
-type carouselProps = {
-  children: string | JSX.Element | JSX.Element[];
+type PropTypes = {
+  img: any;
+  title: string;
+  department: string;
+  phone: string;
+  time: string;
+  location: string;
 };
 
-const CarouselItem = ({ children }: carouselProps) => {
-  return <div className="carouselItemBody">{children}</div>;
+type carouselProps = {
+  props: PropTypes;
+};
+
+const CarouselItem = ({ props }: carouselProps) => {
+  return (
+    <div className="carouselItemBody">
+      <img src={props.img} alt="" />
+      <span>{props.title}</span>
+      <div className="infoBlock">
+        <p>{props.department}</p>
+        <a href={`tel:${props.phone}`}>{props.phone}</a>
+        <p>{props.time}</p>
+        <p>{props.location}</p>
+      </div>
+    </div>
+  );
 };
 
 export default CarouselItem;
